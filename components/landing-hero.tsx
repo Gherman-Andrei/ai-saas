b5c9485b59c77fc658328cbcca6ecf72bb9aa29b
Andrei <andrei30.gherman@gmail.com>
2023-10-26 20:06:16 +0300

    update 3

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -30,7 +30,7 @@ export const LandingHero= () => {
             Create content using AI 10x faster.    
             </div>        
             <div>
-            <Link href ={isSignedIn ? "/dashboard" : "/sing-up"}>
+            <Link href ={isSignedIn ? "/dashboard" : "/sign-up"}>
                 <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
                     Start Generating For Free
                 </Button>
